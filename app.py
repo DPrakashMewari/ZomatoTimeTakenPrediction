7588082d4257c614267825461203f9f913d7cda0
DPrakashMewari <prakash.mewari@yahoo.com>
2023-04-27 00:51:21 +0530

    Notebook

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask 
+from flask import Flask ,request,render_template
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Home"
+
+@app.route("/predictdata",methods=['POST','GET'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        pass
+    
 
 
 if __name__ == "__main__":
